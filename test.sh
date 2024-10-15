7cc5a9554eae6d5641b8de58dbb861c08439ca46
Sebastião Carvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2024-10-15 16:06:31 +0100

    fix in test script to use checker

--- test.sh
+++ test.sh
@@ -2,6 +2,7 @@
 
 run_command="python3 project2.py"
 test_folder="public-tests"
+test_command="./ttp-checker"
 
 declare -a file_base=("t01" "t02" "t03" "t04" "t05" "t06" "t07" "t08" "t09" "t10" "t11" "t12" "t13" "t14" "t15" "t16" "t17" "t18")
 
@@ -14,7 +15,8 @@ for file in "${file_base[@]}"; do
     SECONDS=0    
     $run_command < $file_in > $file_out_test
 
-    if diff $file_out $file_out_test > /dev/null; then
+    result=$($test_command $file_in $file_out_test)
+    if [ "$result" == "OK" ]; then
         duration=$SECONDS
         echo -e "\e[32mTEST PASSED\e[0m : $((duration / 60))m $((duration % 60))s"
     else
